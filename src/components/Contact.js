d73232aeef33a036a32a8cfde1bdab9a2334a327
BubbaBean <brandon.runkel@gmail.com>
2017-09-25 14:14:47 -0400

    responsive parameters set and tested; final steps before publication include cleaning up individual product links, adding social links and final code review

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import Instagram from '../styles/instagram_logo.png';
+import Twitter from '../styles/Twitter-Download-PNG.png';
+import Facebook from '../styles/facebook-icon.png';
+
 export default class Contact extends Component {
   render(){
     return(
       <div className="contact">
         <h1 className="header">Contact Us</h1>
+        <div className="contact_container">
+          <h3 className="contact_title">Here's how you can reach us!</h3>
+            <div className="social_tabs">
+            <img className="social_icon" src={Instagram} alt="instagram logo"/>
+            <img className="social_icon" src={Twitter} alt="twitter logo" />
+            <img className="social_icon" src={Facebook} alt="facebook logo" />
+            </div>
+        </div>
       </div>
     )
   }
